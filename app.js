103369707db3298aee85c524058f4bb7181da059
Gonzalo Mos <gon.mb3@gmail.com>
2022-10-29 14:58:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+
+
+// Display mobile Menu
+
+const mobileMenu = () =>{
+    menu.classList.toggle("is-active")
+    menuLinks.classList.toggle("active")
+}
+
+menu.addEventListener("click", mobileMenu)
+
+
+
+console.log("essaddsasdasdasdasdasdasdasdasdadas")
+
---
+++ img/elon_files/elonnn_files/cookie.js
@@ -0,0 +1,1 @@
+_gfp_s_({"_cookies_":[{"_value_":"ID=93f916b786c559aa-228156cde77a00f2:T=1630951657:RT=1630951657:S=ALNI_MagN9Am88StWXhviwgXMW8vKFfhxw","_expires_":1664647657,"_path_":"/","_domain_":"pixelz.cc"}]});
